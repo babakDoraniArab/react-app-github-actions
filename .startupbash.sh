cf52451fb358a93f22b4d955f24f2f6d5d9c50b3
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-28 16:25:36 +0300

    feat(slack notification): slack notification is added

--- .startupbash.sh
+++ .startupbash.sh
@@ -1,9 +1,6 @@
 echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
-
-
-# npm set-script prepare "husky install"
-# npm run prepare
-
+npx husky install
+npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
 
 # npx husky install
 # npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'

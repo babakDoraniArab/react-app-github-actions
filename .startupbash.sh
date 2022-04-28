ba5f7e82ad2a718a064aeb71cb3ac82408c93f2c
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-28 14:31:41 +0300

    fix(commit): git commitlint and husky works correctly

--- .startupbash.sh
+++ .startupbash.sh
@@ -1,3 +1,21 @@
 echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
-npx husky install
-npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
+
+
+# npm set-script prepare "husky install"
+# npm run prepare
+
+
+# npx husky install
+# npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
+# if the above command does not work do the following command to create a hook 
+# ## Add hook
+# npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
+# # Sometimes above command doesn't work in some command interpreters
+# # You can try other commands below to write npx --no -- commitlint --edit $1
+# # in the commit-msg file.
+# npx husky add .husky/commit-msg \"npx --no -- commitlint --edit '$1'\"
+# # or
+# npx husky add .husky/commit-msg "npx --no -- commitlint --edit $1"
+
+# # or
+# yarn husky add .husky/commit-msg 'yarn commitlint --edit $1'

414226bf8a99d70a983eeb5c0972e895a5ebd99f
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-28 10:27:39 +0300

    feat : zip assets added before releasing

--- release.config.js
+++ release.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-    branches : "master",
-    repositoryUrl:"https://github.com/babakDoraniArab/react-app-github-actions",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        // "@semantic-release/npm",
-        "@semantic-release/github"
-      ]
-  
-}
-//  // "@semantic-release/npm", this is for npm packages 
+  branches: "master",
+  repositoryUrl: "https://github.com/babakDoraniArab/react-app-github-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    // "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
+};
+//  // "@semantic-release/npm", this is for npm packages

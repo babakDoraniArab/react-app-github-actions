69095f2cbcf51714b3d1b33790230a53a4141501
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-28 13:32:29 +0300

    feat(commiting): git commitlint and husky is added and cofigured

--- .startupbash.sh
+++ .startupbash.sh
@@ -1,1 +1,3 @@
 echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
+npx husky install
+npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'

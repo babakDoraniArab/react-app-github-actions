c4d9278d6e0faa937680876817497ae044c46712
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-29 12:31:14 +0300

    feat(open issue): assignees are automated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 
+          Learn React
           babak test coverage 
           test production 
         </a>
